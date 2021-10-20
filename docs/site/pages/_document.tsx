85fc851e0e0bd6ba3dfe2f406bfc362532919f5e
Taufik Oktama <oktamataufik@gmail.com>
2021-10-20 10:58:16 +0700

    [feat] Better SEO

--- docs/site/pages/_document.tsx
+++ docs/site/pages/_document.tsx
@@ -6,6 +6,37 @@ export default class MyDocument extends Document {
             <Html lang="en-US">
                 <Head>
                     <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="image/icons/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="image/icons/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="image/icons/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="image/icons/site.webmanifest" />
+                    <link
+                        rel="mask-icon"
+                        href="image/icons/safari-pinned-tab.svg"
+                        color="#5bbad5"
+                    />
+                    <link rel="shortcut icon" href="image/icons/favicon.ico" />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta
+                        name="msapplication-config"
+                        content="image/icons/browserconfig.xml"
+                    />
+                    <meta name="theme-color" content="#ffffff" />
+
+                    <link
                         rel="preload"
                         type="font/ttf"
                         as="font"
--- docs/site/pages/index.tsx
+++ docs/site/pages/index.tsx
@@ -26,6 +26,8 @@ export default function Page({ deviceType }: PageProps) {
     const meta = {
         title: `${SITE_DATA.name} by ${SITE_DATA.author.name}`,
         description: `${SITE_DATA.name} by ${SITE_DATA.author.name}`,
+        url: "https://typetools.unforma.club",
+        image: `/image/social_card/twitter.png`,
     };
     const { active: activeMedia } = useMediaQuery();
     const [accordion, setAccordion] = useState<Array<Accordion>>([
@@ -81,6 +83,24 @@ export default function Page({ deviceType }: PageProps) {
                 <title>{meta.title}</title>
                 <meta name="title" content={meta.title} />
                 <meta name="description" content={meta.description} />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content={meta.url} />
+                <meta property="og:title" content={meta.title} />
+                <meta property="og:description" content={meta.title} />
+                <meta
+                    property="og:image"
+                    content={`${meta.url}${meta.image}`}
+                />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content={meta.url} />
+                <meta property="twitter:title" content={meta.title} />
+                <meta property="twitter:description" content={meta.title} />
+                <meta
+                    property="twitter:image"
+                    content={`${meta.url}${meta.image}`}
+                />
             </NextHead>
 
             {isMobile && (
