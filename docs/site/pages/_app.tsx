716b74a3c82096ecaf344264eb848ec0fccb6946
Taufik Oktama <oktamataufik@gmail.com>
2021-10-22 13:10:11 +0700

    [feat] Remove birds on client
    notes: will be conditional while user idle in the future

--- docs/site/pages/_app.tsx
+++ docs/site/pages/_app.tsx
@@ -1,10 +1,10 @@
 import "styles/global.scss";
-import NextDynamic from "next/dynamic";
+// import NextDynamic from "next/dynamic";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { ProviderFonts } from "libs/context/ContextFonts";
 
-const Birds = NextDynamic(() => import("components/Birds"), { ssr: false });
+// const Birds = NextDynamic(() => import("components/Birds"), { ssr: false });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
@@ -16,7 +16,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             >
                 <ProviderFonts>
                     <Component {...pageProps} />
-                    <Birds />
+                    {/* <Birds /> */}
                 </ProviderFonts>
             </ThemeProvider>
         </>
